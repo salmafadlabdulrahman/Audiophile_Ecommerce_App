3477a5ae38a03c6579a251e7a00710e33cfcc4de
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-12-21 21:09:50 +0200

    update the products section

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -3,8 +3,8 @@ import CategoryCard from "./CategoryCard";
 
 const Products = () => {
   return (
-    <div className="mt-[4em] pb-10 lg:flex justify-center ">
-      <div className="flex items-center gap-3">
+    <div className="mt-[4em] pb-10 lg:flex justify-center  ">
+      <div className="flex flex-col items-center md:flex-row gap-3">
         {products.map((product) => (
           <CategoryCard category={product.category} image={product.image} />
         ))}
