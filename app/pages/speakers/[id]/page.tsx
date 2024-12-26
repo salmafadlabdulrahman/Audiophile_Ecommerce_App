ac33914de67325436f2dd9ea33ea40c66643595a
salmafadlabdulrahman <salmafadlabdulrahman@gmail.com>
2024-12-26 21:00:33 +0200

    update dynamic routes

--- app/pages/speakers/[id]/page.tsx
+++ app/pages/speakers/[id]/page.tsx
@@ -2,9 +2,9 @@ import data from "@/products.json"
 import Image from "next/image";
 
 const page = async({ params }: { params: { id: string } }) => {
-  const {id} = params
+  const awaitedParams = await params;
   
-  const item = data.products.find((product) => product.id === parseInt(id));
+  const item = data.products.find((product) => product.id === parseInt(awaitedParams.id));
   return (
     <div>
       <h1>{item?.name}</h1>
